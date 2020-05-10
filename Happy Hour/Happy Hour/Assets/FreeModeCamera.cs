3c369778fb73c46469ba9a37a371e4566a8c70f0
Weeaboo420 <43264625+Weeaboo420@users.noreply.github.com>
2020-05-10 11:37:51 +0200

    Initial commit

---
+++ Happy Hour/Happy Hour/Assets/FreeModeCamera.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FreeModeCamera : MonoBehaviour
+{
+    private float moveSpeed, speedMultiplier = 1f;
+    private float horizontalMouseSpeed = 1.2f, verticalMouseSpeed = 1.2f,  minFov = 60, maxFov = 100, fovIncrement = 2f;
+    private int inputs = 0;
+    private bool movingCamera = true, sprinting = false;
+    private Camera _camera;
+
+    public GameObject waterTile;
+    public bool shouldFollow;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void HandleSpeed()
+    {
+        //Limit max speed based on the amount of inputs. This prevents going double the normal speed when moving diagonally.
+        if (inputs >= 2)
+        {
+            if (!sprinting)
+            {
+                speedMultiplier = 0.5f;
+            } else
+            {
+                speedMultiplier = 0.65f;
+            }
+        }
+
+        else if (inputs < 2)
+        {
+            if (!sprinting)
+            {
+                speedMultiplier = 1f;
+            }
+        }
+    }
+
+    void Update()
+    {
+
+        if (shouldFollow)
+        {
+            Vector3 waterTilePos = waterTile.transform.position;
+            waterTilePos.x = transform.position.x;
+            waterTilePos.z = transform.position.z;
+            waterTile.transform.position = waterTilePos;
+        }
+
+        //Basic setup
+        movingCamera = Input.GetMouseButton(1);
+        moveSpeed = 4.5f * speedMultiplier;
+        sprinting = Input.GetKey(KeyCode.LeftShift);
+
+        switch(sprinting)
+        {
+            case true:
+                if(inputs < 2)
+                {
+                    speedMultiplier = 1.7f;                    
+                }
+                break;
+
+            case false:
+                if(inputs < 2)
+                {
+                    speedMultiplier = 1f;
+                }
+                break;
+        }
+
+        //Handle sprinting
+        if (Input.GetKeyDown(KeyCode.LeftShift) && inputs < 2)
+        {
+            speedMultiplier = 1.3f;            
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speedMultiplier = 1f;           
+        }
+
+        //Handle fov changes
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            //Scollwheel down
+            if (_camera.fieldOfView < maxFov)
+            {
+                _camera.fieldOfView += fovIncrement;
+            }
+
+            if(_camera.fieldOfView > maxFov)
+            {
+                _camera.fieldOfView = maxFov;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //Scollwheel up
+            if (_camera.fieldOfView > minFov)
+            {
+                _camera.fieldOfView -= fovIncrement;
+            }
+
+            if (_camera.fieldOfView < minFov)
+            {
+                _camera.fieldOfView = minFov;
+            }
+        }
+
+        //Handle camera rotation and clamping
+        Vector3 myRot = transform.rotation.eulerAngles;
+        if (movingCamera)
+        {
+            myRot.y += Input.GetAxisRaw("Mouse X") * horizontalMouseSpeed;
+            myRot.x -= Input.GetAxisRaw("Mouse Y") * verticalMouseSpeed;            
+        }
+        transform.rotation = Quaternion.Euler(myRot);
+        Quaternion myQRot = transform.rotation;
+        myQRot.x = Mathf.Clamp(myQRot.x, -90, 90);
+        transform.rotation = myQRot;
+
+
+        //Limit max speed based on the amount of inputs. This prevents going double the normal speed when moving diagonally.
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            inputs += 1;
+            if(inputs > 4)
+            {
+                inputs = 4;
+            }
+
+            HandleSpeed();
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            inputs -= 1;
+            if(inputs < 0 )
+            {
+                inputs = 0;
+            }
+
+            HandleSpeed();
+        }
+
+
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(transform.forward *  Time.deltaTime  *  moveSpeed, Space.World);
+        }
+
+        else if (Input.GetKey(KeyCode.S)  && !Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-transform.forward * Time.deltaTime * moveSpeed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-transform.right * Time.deltaTime * moveSpeed, Space.World);
+        }
+
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(transform.right * Time.deltaTime * moveSpeed, Space.World);
+        }
+
+    }
+}
---
+++ Happy Hour/Happy Hour/Assets/TerrainGenerator.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TerrainGenerator : MonoBehaviour
+{
+    private MeshFilter _meshFilter;    
+    private int chunkWidth = 16, chunkHeight = 16;
+
+    void Start()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        Generate();
+        //_meshRenderer.material = (Material)AssetDatabase.LoadAssetAtPath<Material>("Assets/StockMaterial.mat");
+    }
+
+    private void Generate()
+    {
+        int blockCount = 0;
+        Vector3[] vertices = new Vector3[8 * 3 * chunkWidth * chunkWidth * chunkHeight];
+        int[] triangles = new int[36 * chunkWidth * chunkWidth * chunkHeight];
+        Vector2[] uvs = new Vector2[8 * 3 * chunkWidth * chunkWidth * chunkHeight];
+
+        bool[,,] isOpaqueBlock = new bool[chunkWidth, chunkHeight, chunkWidth];
+
+        //First pass, for boolean array
+        for (int y = 0; y < chunkHeight; y++)
+        {
+            for (int z = 0; z < chunkWidth; z++)
+            {
+                for (int x = 0; x < chunkWidth; x++)
+                {
+                    isOpaqueBlock[x, y, z] = true;
+                }
+            }
+        }
+
+
+        //Second pass, actual mesh generation
+        for (int x = 0; x < chunkWidth; x++)
+        {
+            for (int y = 0; y < chunkHeight; y++)
+            {
+                for (int z = 0; z < chunkWidth; z++)
+                {
+                    /* 
+                    * Assign vertices
+                    * Assign triangles
+                    */
+
+                    #region verts
+
+                    //First (y)
+                    vertices[0 + (blockCount * 24)] = new Vector3(0 - x, 0 + y, 0 + z);
+                    vertices[1 + (blockCount * 24)] = new Vector3(0 - x, 0 + y, 1 + z);
+                    vertices[2 + (blockCount * 24)] = new Vector3(1 - x, 0 + y, 1 + z);
+                    vertices[3 + (blockCount * 24)] = new Vector3(1 - x, 0 + y, 0 + z);
+
+                    vertices[4 + (blockCount * 24)] = new Vector3(0 - x, -1 + y, 0 + z);
+                    vertices[5 + (blockCount * 24)] = new Vector3(0 - x, -1 + y, 1 + z);
+                    vertices[6 + (blockCount * 24)] = new Vector3(1 - x, -1 + y, 1 + z);
+                    vertices[7 + (blockCount * 24)] = new Vector3(1 - x, -1 + y, 0 + z);
+
+                    //Second (z)
+                    vertices[8 + (blockCount * 24)] = new Vector3(0 - x, 0 + y, 0 + z);
+                    vertices[9 + (blockCount * 24)] = new Vector3(0 - x, 0 + y, 1 + z);
+                    vertices[10 + (blockCount * 24)] = new Vector3(1 - x, 0 + y, 1 + z);
+                    vertices[11 + (blockCount * 24)] = new Vector3(1 - x, 0 + y, 0 + z);
+
+                    vertices[12 + (blockCount * 24)] = new Vector3(0 - x, -1 + y, 0 + z);
+                    vertices[13 + (blockCount * 24)] = new Vector3(0 - x, -1 + y, 1 + z);
+                    vertices[14 + (blockCount * 24)] = new Vector3(1 - x, -1 + y, 1 + z);
+                    vertices[15 + (blockCount * 24)] = new Vector3(1 - x, -1 + y, 0 + z);
+
+                    //Third (x)
+                    vertices[16 + (blockCount * 24)] = new Vector3(0 - x, 0 + y, 0 + z);
+                    vertices[17 + (blockCount * 24)] = new Vector3(0 - x, 0 + y, 1 + z);
+                    vertices[18 + (blockCount * 24)] = new Vector3(1 - x, 0 + y, 1 + z);
+                    vertices[19 + (blockCount * 24)] = new Vector3(1 - x, 0 + y, 0 + z);
+
+                    vertices[20 + (blockCount * 24)] = new Vector3(0 - x, -1 + y, 0 + z);
+                    vertices[21 + (blockCount * 24)] = new Vector3(0 - x, -1 + y, 1 + z);
+                    vertices[22 + (blockCount * 24)] = new Vector3(1 - x, -1 + y, 1 + z);
+                    vertices[23 + (blockCount * 24)] = new Vector3(1 - x, -1 + y, 0 + z);
+
+                    #endregion
+
+                    //Top faces (y+)
+                    triangles[0 + (blockCount * 36)] = 0 + (blockCount * 24);
+                    triangles[1 + (blockCount * 36)] = 1 + (blockCount * 24);
+                    triangles[2 + (blockCount * 36)] = 2 + (blockCount * 24);
+                    triangles[3 + (blockCount * 36)] = 2 + (blockCount * 24);
+                    triangles[4 + (blockCount * 36)] = 3 + (blockCount * 24);
+                    triangles[5 + (blockCount * 36)] = 0 + (blockCount * 24);
+
+                    //Front faces (-z)
+                    if (z - 1 > -1)
+                    {
+                        if (!isOpaqueBlock[x, y, z - 1])
+                        {
+                            triangles[6 + (blockCount * 36)] = 12 + (blockCount * 24);
+                            triangles[7 + (blockCount * 36)] = 8 + (blockCount * 24);
+                            triangles[8 + (blockCount * 36)] = 11 + (blockCount * 24);
+                            triangles[9 + (blockCount * 36)] = 11 + (blockCount * 24);
+                            triangles[10 + (blockCount * 36)] = 15 + (blockCount * 24);
+                            triangles[11 + (blockCount * 36)] = 12 + (blockCount * 24);
+                        }
+                    }
+                    else
+                    {
+                        triangles[6 + (blockCount * 36)] = 12 + (blockCount * 24);
+                        triangles[7 + (blockCount * 36)] = 8 + (blockCount * 24);
+                        triangles[8 + (blockCount * 36)] = 11 + (blockCount * 24);
+                        triangles[9 + (blockCount * 36)] = 11 + (blockCount * 24);
+                        triangles[10 + (blockCount * 36)] = 15 + (blockCount * 24);
+                        triangles[11 + (blockCount * 36)] = 12 + (blockCount * 24);
+                    }
+
+                    //Left faces (-x)
+                    if (x + 1 < chunkWidth)
+                    {
+                        if (!isOpaqueBlock[x + 1, y, z])
+                        {
+                            triangles[12 + (blockCount * 36)] = 21 + (blockCount * 24);
+                            triangles[13 + (blockCount * 36)] = 17 + (blockCount * 24);
+                            triangles[14 + (blockCount * 36)] = 16 + (blockCount * 24);
+                            triangles[15 + (blockCount * 36)] = 16 + (blockCount * 24);
+                            triangles[16 + (blockCount * 36)] = 20 + (blockCount * 24);
+                            triangles[17 + (blockCount * 36)] = 21 + (blockCount * 24);
+                        }
+                    }
+                    else
+                    {
+                        triangles[12 + (blockCount * 36)] = 21 + (blockCount * 24);
+                        triangles[13 + (blockCount * 36)] = 17 + (blockCount * 24);
+                        triangles[14 + (blockCount * 36)] = 16 + (blockCount * 24);
+                        triangles[15 + (blockCount * 36)] = 16 + (blockCount * 24);
+                        triangles[16 + (blockCount * 36)] = 20 + (blockCount * 24);
+                        triangles[17 + (blockCount * 36)] = 21 + (blockCount * 24);
+                    }
+
+                    //Right faces (+x)
+                    if (x - 1 > -1)
+                    {
+                        if (!isOpaqueBlock[x - 1, y, z])
+                        {
+                            triangles[18 + (blockCount * 36)] = 23 + (blockCount * 24);
+                            triangles[19 + (blockCount * 36)] = 19 + (blockCount * 24);
+                            triangles[20 + (blockCount * 36)] = 18 + (blockCount * 24);
+                            triangles[21 + (blockCount * 36)] = 18 + (blockCount * 24);
+                            triangles[22 + (blockCount * 36)] = 22 + (blockCount * 24);
+                            triangles[23 + (blockCount * 36)] = 23 + (blockCount * 24);
+                        }
+                    }
+                    else
+                    {
+                        triangles[18 + (blockCount * 36)] = 23 + (blockCount * 24);
+                        triangles[19 + (blockCount * 36)] = 19 + (blockCount * 24);
+                        triangles[20 + (blockCount * 36)] = 18 + (blockCount * 24);
+                        triangles[21 + (blockCount * 36)] = 18 + (blockCount * 24);
+                        triangles[22 + (blockCount * 36)] = 22 + (blockCount * 24);
+                        triangles[23 + (blockCount * 36)] = 23 + (blockCount * 24);
+                    }
+
+                    //Back faces (+z)
+                    if (z + 1 < chunkWidth)
+                    {
+                        if (!isOpaqueBlock[x, y, z + 1])
+                        {
+                            triangles[24 + (blockCount * 36)] = 14 + (blockCount * 24);
+                            triangles[25 + (blockCount * 36)] = 10 + (blockCount * 24);
+                            triangles[26 + (blockCount * 36)] = 9 + (blockCount * 24);
+                            triangles[27 + (blockCount * 36)] = 9 + (blockCount * 24);
+                            triangles[28 + (blockCount * 36)] = 13 + (blockCount * 24);
+                            triangles[29 + (blockCount * 36)] = 14 + (blockCount * 24);
+                        }
+                    }
+                    else
+                    {
+                        triangles[24 + (blockCount * 36)] = 14 + (blockCount * 24);
+                        triangles[25 + (blockCount * 36)] = 10 + (blockCount * 24);
+                        triangles[26 + (blockCount * 36)] = 9 + (blockCount * 24);
+                        triangles[27 + (blockCount * 36)] = 9 + (blockCount * 24);
+                        triangles[28 + (blockCount * 36)] = 13 + (blockCount * 24);
+                        triangles[29 + (blockCount * 36)] = 14 + (blockCount * 24);
+                    }
+
+                    //Bottom faces (-y)
+                    triangles[30 + (blockCount * 36)] = 7 + (blockCount * 24);
+                    triangles[31 + (blockCount * 36)] = 6 + (blockCount * 24);
+                    triangles[32 + (blockCount * 36)] = 5 + (blockCount * 24);
+                    triangles[33 + (blockCount * 36)] = 5 + (blockCount * 24);
+                    triangles[34 + (blockCount * 36)] = 4 + (blockCount * 24);
+                    triangles[35 + (blockCount * 36)] = 7 + (blockCount * 24);
+
+                    //Top face, pair 1
+                    uvs[0 + (blockCount * 24)] = new Vector2(1, 1);
+                    uvs[1 + (blockCount * 24)] = new Vector2(1, 0);
+                    uvs[2 + (blockCount * 24)] = new Vector2(0, 0);
+                    uvs[3 + (blockCount * 24)] = new Vector2(0, 1);
+
+                    //Bottom face, pair 1
+                    uvs[4 + (blockCount * 24)] = new Vector2(1, 0);
+                    uvs[5 + (blockCount * 24)] = new Vector2(1, 1);
+                    uvs[6 + (blockCount * 24)] = new Vector2(0, 1);
+                    uvs[7 + (blockCount * 24)] = new Vector2(0, 0);
+
+                    //Front face, pair 2
+                    uvs[8 + (blockCount * 24)] = new Vector2(1, 0);
+                    uvs[9 + (blockCount * 24)] = new Vector2(1, 1);
+                    uvs[10 + (blockCount * 24)] = new Vector2(0, 1);
+                    uvs[11 + (blockCount * 24)] = new Vector2(0, 0);
+
+                    //Back face, pair 2
+                    uvs[12 + (blockCount * 24)] = new Vector2(1, 1);
+                    uvs[13 + (blockCount * 24)] = new Vector2(1, 0);
+                    uvs[14 + (blockCount * 24)] = new Vector2(0, 0);
+                    uvs[15 + (blockCount * 24)] = new Vector2(0, 1);
+
+                    //Left face, pair 3
+                    uvs[16 + (blockCount * 24)] = new Vector2(1, 1);
+                    uvs[17 + (blockCount * 24)] = new Vector2(0, 1);
+                    uvs[18 + (blockCount * 24)] = new Vector2(1, 1);
+                    uvs[19 + (blockCount * 24)] = new Vector2(0, 1);
+
+                    //Right face, pair 3
+                    uvs[20 + (blockCount * 24)] = new Vector2(1, 0);
+                    uvs[21 + (blockCount * 24)] = new Vector2(0, 0);
+                    uvs[22 + (blockCount * 24)] = new Vector2(1, 0);
+                    uvs[23 + (blockCount * 24)] = new Vector2(0, 0);
+
+                    blockCount += 1;
+
+                }
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+
+        mesh.RecalculateTangents();
+        mesh.RecalculateNormals();        
+
+        _meshFilter.mesh = mesh;        
+
+    }
+
+}
